52fc2d9d7f78c1e4bc355b02c5558ad8812c58c4
clon <guyclon.git@gmail.com>
2019-02-11 20:52:48 +0900

    アイテム回収の追加

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
