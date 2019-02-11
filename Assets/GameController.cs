23919001ebd5604d4773bc865298fa5692faf58b
clon <guyclon.git@gmail.com>
2019-02-11 21:14:45 +0900

    アイテムの残り数を画面に表示

---
+++ Assets/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text scoreLabel;
+    void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+    }
+}
