f66bb555f17c0dfba1c6924e984c665f0f72e7f0
clon <guyclon.git@gmail.com>
2019-02-11 20:19:43 +0900

    動くプレイヤーを追加

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+}
