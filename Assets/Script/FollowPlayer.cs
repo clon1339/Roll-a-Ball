95204c5897b8fb88b320b3901103b1f9d63e71f5
clon <guyclon.git@gmail.com>
2019-02-11 20:35:43 +0900

    プレイヤーに追跡するカメラを追加

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+       offset = GetComponent<Transform>().position - target.position;
+    }
+
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position + offset;
+    }
+}
