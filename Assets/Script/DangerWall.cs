54595153c93c54e490d5d74a8d10fe6d879423f4
clon <guyclon.git@gmail.com>
2019-02-11 21:42:24 +0900

    障害物に触れたらリスタート機能追加

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour
+{
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
