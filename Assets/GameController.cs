15000e7b4f9af8e5009711e9df013bc103bc34be
clon <guyclon.git@gmail.com>
2019-02-11 21:21:21 +0900

    ゲームクリアの追加

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+
     void Update()
     {
         int count = GameObject.FindGameObjectsWithTag("Item").Length;
         scoreLabel.text = count.ToString();
+
+        if(count == 0)
+        {
+            winnerLabelObject.SetActive(true);
+        }
     }
 }
